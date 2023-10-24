9d1e143f360ea8eb46580df4b70bb3b0a535ae1a
stevenduong2597 <65508238+stevenduong2597@users.noreply.github.com>
2023-10-23 23:42:55 -0500

    delete locationManager

--- Coog Path/Features/Home/Screens/LocationManager.swift
+++ Coog Path/Features/Home/Screens/LocationManager.swift
@@ -11,23 +11,32 @@ import CoreLocation
 class LocationManager: NSObject {
     
 
-//    class CLLocationManagerDelegate{
-//        
-//    }
-//    class ObservableObject{
-//        
-//    }
-//    @Published var region = MKCoordinateRegion()
-//
-//    private let manager = CLLocationManager()
-//    
-//    override init() {
-//            super.init()
-//            manager.delegate = self
-//            manager.desiredAccuracy = kCLLocationAccuracyBest
-//            manager.requestWhenInUseAuthorization()
-//            manager.startUpdatingLocation()
-//    }
-//    .startUpdatingLocation()
+    class CLLocationManagerDelegate{
+        
+    }
+    class ObservableObject{
+        
+    }
+    @Published var region = MKCoordinateRegion()
+
+    private let manager = CLLocationManager()
+    
+    override init() {
+            super.init()
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+    }
+    .startUpdatingLocation()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            locations.last.map {
+                region = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(latitude: $0.coordinate.latitude, longitude: $0.coordinate.longitude),
+                    span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+                )
+            }
+        }
 }
 
