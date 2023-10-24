b3ed9ceeed414f699ccf8d32795217c38c89e0e0
stevenduong2597 <65508238+stevenduong2597@users.noreply.github.com>
2023-10-23 23:22:12 -0500

    Added HomeScreen and LocationManager

--- Coog Path/Features/Home/Screens/HomeScreen.swift
+++ Coog Path/Features/Home/Screens/HomeScreen.swift
@@ -6,10 +6,79 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HomeScreen: View {
+    @State private var fromLocation: String = ""
+    @State private var toLocation: String = ""
+    @State private var hiddenButton: Bool = true
+    
     var body: some View {
-        Text("Home Screen")
+        VStack {
+            //Title
+            HStack {
+                Text("Coog Path")
+                    .font(.largeTitle)
+                    .bold()
+                Spacer()
+            }
+            Spacer()
+            
+            //From stack
+            VStack {
+                HStack{
+                    Text("From")
+                        .bold()
+                        .font(.system(size:20))
+                    Spacer()
+                }
+                TextField(
+                    "Your Current Location",
+                    text: $fromLocation
+                )
+                .textFieldStyle(.roundedBorder)
+                
+            }
+            
+            //Going to stack
+            VStack{
+                HStack{
+                    Text("Going to")
+                        .bold()
+                        .font(.system(size:20))
+                    Spacer()
+                }
+                
+                TextField(
+                    "Search building",
+                    text: $toLocation
+                )
+                .textFieldStyle(.roundedBorder)
+            }
+            .padding(.bottom)
+            Spacer()
+            
+            //Map stack
+            VStack{
+                Map()
+                    .clipShape(.rect(cornerRadius: 16))
+            }
+            
+            Spacer()
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Go Coog")
+                    .font(.title2)
+                    .bold()
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                    .padding(.horizontal,20)
+                    .foregroundStyle(.white)
+             
+            })
+            .background(RoundedRectangle(cornerRadius: 16).fill(Color("MainColor")))
+            Spacer()
+        }
+        .padding()
     }
 }
 
---
+++ Coog Path/Features/Home/Screens/LocationManager.swift
@@ -0,0 +1,33 @@
+//
+//  LocationManager.swift
+//  Coog Path
+//
+//  Created by Steven Duong on 10/22/23.
+//
+
+import MapKit
+import CoreLocation
+
+class LocationManager: NSObject {
+    
+
+//    class CLLocationManagerDelegate{
+//        
+//    }
+//    class ObservableObject{
+//        
+//    }
+//    @Published var region = MKCoordinateRegion()
+//
+//    private let manager = CLLocationManager()
+//    
+//    override init() {
+//            super.init()
+//            manager.delegate = self
+//            manager.desiredAccuracy = kCLLocationAccuracyBest
+//            manager.requestWhenInUseAuthorization()
+//            manager.startUpdatingLocation()
+//    }
+//    .startUpdatingLocation()
+}
+
