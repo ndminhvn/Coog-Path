52d5f1533527d7c01db8e3215a8cb1e9bf021fe4
Minh Nguyen <vn.ndminh@gmail.com>
2023-10-28 19:09:16 -0500

    Sketch for building detail screen

--- Coog Path/Features/Buildings/Screens/BuildingDetailScreen.swift
+++ Coog Path/Features/Buildings/Screens/BuildingDetailScreen.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct BuildingDetailScreen: View {
+    var building: Building
+
     var body: some View {
-        Text("Building Detail Screen")
+        VStack {
+            VStack {
+                Rectangle()
+                    .foregroundStyle(.brown)
+                    .frame(maxHeight: 200)
+            }
+            Spacer()
+            VStack (alignment: .leading) {
+                VStack(alignment: .center) {
+                    Text(building.Name)
+                        .bold()
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                    Text("Address")
+                }
+                Spacer()
+                Section {
+                    Text("Sample room numbers:")
+                        .bold()
+                        .font(.headline)
+                }
+                Spacer()
+                Section {
+                    Text("Your classes in this building:")
+                        .bold()
+                        .font(.headline)
+                }
+                Spacer()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .background(Color.blue.opacity(0.75))
+            Spacer()
+        }
+        .padding()
+        .padding(.top, -30)
     }
 }
 
 #Preview {
-    BuildingDetailScreen()
+    BuildingDetailScreen(building: Building(Number: "0001", Abbr: "PGH", Name: "Philip"))
 }
