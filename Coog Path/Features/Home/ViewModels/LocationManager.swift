a8a5537c66348abee81ee19cc2e837a949cc7356
stevenduong2597 <65508238+stevenduong2597@users.noreply.github.com>
2023-11-02 10:38:13 -0500

    Added search service
    Show button when user click on valid building name
    Show marker at destination location
    Show route

--- Coog Path/Features/Home/ViewModels/LocationManager.swift
+++ Coog Path/Features/Home/ViewModels/LocationManager.swift
@@ -6,17 +6,22 @@
 //
 
 import Foundation
+import MapKit
 import _MapKit_SwiftUI
 
 final class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
-    @Published var region = MapCameraPosition.region(MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 29.766083, longitude: -95.358810),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
-    
+//    @Published var myRegion = MapCameraPosition.region(MKCoordinateRegion(
+//        center: CLLocationCoordinate2D(latitude: 29.766083, longitude: -95.358810),
+//        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
+    @Published var myRegion: MKCoordinateRegion = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 29.766083, longitude: -95.358810),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
     // UH Main center coordinate
-    @Published var position: MapCameraPosition = .camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 29.72001, longitude: -95.34207), distance: 1000))
+    @Published var myPosition: MapCameraPosition = .camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 29.72001, longitude: -95.34207), distance: 1000))
+    @Published var myLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 29.72001, longitude: -95.34207)
 //    @Published var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
     override init() {
@@ -52,13 +57,34 @@ extension LocationManager: CLLocationManagerDelegate {
         print("Something went wrong: \(error)")
     }
     
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        locationManager.stopUpdatingLocation()
+//        locations.last.map {
+//            myRegion = MapCameraPosition.region(MKCoordinateRegion(
+//                center: $0.coordinate,
+//                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+//            ))
+//        }
+//    }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         locationManager.stopUpdatingLocation()
-        locations.last.map {
-            region = MapCameraPosition.region(MKCoordinateRegion(
-                center: $0.coordinate,
-                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
-            ))
+        
+        if let lastLocation = locations.last {
+            myRegion = MKCoordinateRegion(
+                center: lastLocation.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
         }
     }
+    
 }
+//extension CLLocationCoordinate2D{
+//    static var myLocation: CLLocationCoordinate2D{
+//        return .init(latitude: 29.766083, longitude: -95.358810)
+//    }
+//}
+//extension MKCoordinateRegion{
+//    static var myRegion: MKCoordinateRegion{
+//        return .init(center: .myLocation, latitudinalMeters: 10000, longitudinalMeters: 10000)
+//    }
+//}
